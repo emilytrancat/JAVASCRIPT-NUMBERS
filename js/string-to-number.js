3ba4771b55be5a0c6076e1d7b0dbadde9ee1c655
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-20 13:44:25 -0500

    Convert Strings to Numbers

--- js/string-to-number.js
+++ js/string-to-number.js
@@ -1,6 +1,17 @@
-const HTMLBadges = prompt('How many HTML badges do you have?');
-const CSSBadges = prompt('How many CSS badges do you have?');
+// const HTMLBadges = prompt('How many HTML badges do you have?');
+// const CSSBadges = prompt('How many CSS badges do you have?');
 
-const totalBadges = HTMLBadges + CSSBadges;
-console.log(totalBadges);
-console.log(typeof HTMLBadges, typeof CSSBadges);
+// // const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+// const totalBadges = +HTMLBadges + +CSSBadges;
+
+// console.log(totalBadges);
+
+// console.log( typeof parseInt(HTMLBadges), typeof parseInt(CSSBadges) );
+
+// const hi = prompt('say a number');
+// const hichu = prompt('say another number');
+// const piTotal = parseFloat(hi) + parseFloat(hichu);
+// console.log(piTotal);
+
+const pi = prompt('What is Pi?');
+console.log( +pi === 3.14);
