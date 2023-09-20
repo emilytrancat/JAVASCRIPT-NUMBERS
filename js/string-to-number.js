93f2ec6df92e73cdf7c4e3e46dca2e5e5cd0b74a
emilytrancat <123094758+emilytrancat@users.noreply.github.com>
2023-09-20 13:01:17 -0500

    Treating Strings as Numbers

--- js/string-to-number.js
+++ js/string-to-number.js
@@ -2,5 +2,5 @@ const HTMLBadges = prompt('How many HTML badges do you have?');
 const CSSBadges = prompt('How many CSS badges do you have?');
 
 const totalBadges = HTMLBadges + CSSBadges;
-console.log(totalBadges)
-
+console.log(totalBadges);
+console.log(typeof HTMLBadges, typeof CSSBadges);
